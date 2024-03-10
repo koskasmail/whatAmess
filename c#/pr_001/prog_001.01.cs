a95724d098c109040ac93a9a7e7accc51d2cf209
kessler yaron <koskasmail@gmail.com>
2024-03-10 17:04:06 +0200

    Create prog_001.01.cs

---
+++ c#/pr_001/prog_001.01.cs
@@ -0,0 +1,19 @@
+
+using System; ///----[name space]----///
+
+public class ProgramBase ///----[class]----///
+{
+    public static void Main(string[] args)
+    {
+        ///----[Text]----///
+        Console.WriteLine("Welcome to the Code Playground");
+
+        ///----[number]----///
+        Console.WriteLine(404);
+
+        ///----[witout 'using System' namespace ]----///
+        System.Console.WriteLine("text witout 'using System' namespace ");
+        System.Console.WriteLine(156);
+
+    }
+}
