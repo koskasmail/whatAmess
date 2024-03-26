0c7e101c3f02640d37c2212571d4ccfacebbe180
kessler yaron <koskasmail@gmail.com>
2024-03-26 17:16:47 +0200

    Create salary.cs

---
+++ c#/single/salary.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int salary = Convert.ToInt32(Console.ReadLine());  
+          //your code goes here
+          salary *= 12;
+          System.Console.WriteLine(salary);
+        }
+    }
+}
