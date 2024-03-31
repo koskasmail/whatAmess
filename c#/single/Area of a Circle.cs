17c024712b7c941b4ff6e085ceddbc2cd1866dfb
kessler yaron <koskasmail@gmail.com>
2024-03-31 11:08:09 +0300

    Update Area of a Circle.cs

--- c#/single/Area of a Circle.cs
+++ c#/single/Area of a Circle.cs
@@ -12,9 +12,14 @@ namespace SoloLearn
         {
             const double pi = 3.14;
             double radius;
-            
+            double circleArea;
+
             //your code goes here
-            
+            System.Console.WriteLine("Enter a radius number: ");
+            radius = Convert.ToDouble(System.Console.ReadLine());
+            System.Console.WriteLine("You have enter: {0}", radius);
+            circleArea = (radius * radius) * pi;
+            System.Console.WriteLine("fnCircleArea Area is {0}", circleArea);           
         }
     }
 }
