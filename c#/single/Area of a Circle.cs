b2a585ecfaf9703ba6f96860f2f943fcc0d8eb0c
kessler yaron <koskasmail@gmail.com>
2024-03-31 09:22:39 +0300

    Create Area of a Circle.cs

---
+++ c#/single/Area of a Circle.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius;
+            
+            //your code goes here
+            
+        }
+    }
+}
