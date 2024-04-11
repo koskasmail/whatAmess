f1c262854a5d43f6cee0a2d27f9728526214b334
kessler yaron <koskasmail@gmail.com>
2024-04-11 16:52:01 +0300

    Create The default Case.cs

---
+++ c#/single/The default Case.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            /*
+            1 - Easy
+            2 - Medium
+            3 - Hard
+            other - "Invalid option"
+            */
+
+            //your code goes here
+            
+        }
+    }
+}
