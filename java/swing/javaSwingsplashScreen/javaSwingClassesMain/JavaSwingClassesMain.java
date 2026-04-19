ae0557527c6dfbcba244e0a70fe373452ef0e07f
kessler yaron <koskasmail@gmail.com>
2026-04-19 08:11:14 +0300

    4.5

--- java/swing/javaSwingsplashScreen/javaSwingClassesMain/JavaSwingClassesMain.java
+++ java/swing/javaSwingsplashScreen/javaSwingClassesMain/JavaSwingClassesMain.java
@@ -1,12 +1,15 @@
 package javaSwingClassesMain;
 
-import wellcome_02.WellcomeMain;
+// import wellcome_01.WellcomeMain;
+// import wellcome_02.WellcomeMain;
+// import wellcome_03.WellcomeMain;
+import wellcome_04.WellcomeMain;
 
 public class JavaSwingClassesMain {
 
 	public JavaSwingClassesMain() {
-//		WellcomeMain wn1 = new WellcomeMain();
-		WellcomeMain wm2 = new WellcomeMain();
+		WellcomeMain wm = new WellcomeMain();
+
 	}
 
 	public static void main(String[] args) {
